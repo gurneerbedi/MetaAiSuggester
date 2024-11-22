9b12247058aea33e99db269b01f47a7db0ca67c6
bzhang98 <105823390+bzhang98@users.noreply.github.com>
2024-11-22 10:09:52 -0600

    update questions path

--- src/api/backend.js
+++ src/api/backend.js
@@ -24,7 +24,7 @@ const getProducts = async () => {
 
 const getQuestions = async () => {
   try {
-    const { data } = await axios.get(`${backendUrl}/`);
+    const { data } = await axios.get(`${backendUrl}/questions`);
     return data;
   } catch (error) {
     console.error("Could not get quiz questions:", error);
