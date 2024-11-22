3e68e300cf34d0727464b0d83101f81c6580301f
alexandra.lionga <174167678+alexandra-lionga@users.noreply.github.com>
2024-11-22 06:47:37 -0500

    creating sucess stories page

---
+++ src/Pages/SuccessStories/SuccessStories.jsx
@@ -0,0 +1,12 @@
+import "./SuccessStories.scss";
+
+const SuccessStories = async ()=>{
+    return(
+        <div className="success-stories">
+            <h1>Success Stories</h1>
+
+        </div>
+    )
+}
+
+export default SuccessStories;
