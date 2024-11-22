77243342eeca16862a00d95539259692801cea03
alexandra.lionga <174167678+alexandra-lionga@users.noreply.github.com>
2024-11-22 13:51:28 -0500

    putting back the useEffect back to results page

--- src/Pages/Results/Results.jsx
+++ src/Pages/Results/Results.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import ProductCard from "../../components/ProductCard/ProductCard.jsx";
 import { Typography, Container } from "@mui/material";
 import { motion } from "framer-motion";
