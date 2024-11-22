6b477bf886b31d5f6961bd6e1df3eb1221f1eb34
Emily Schur <ejschur@gmail.com>
2024-11-21 23:18:11 -0800

    Style results page, add animation, meta logo, tab name

--- src/Pages/Results/Results.jsx
+++ src/Pages/Results/Results.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import ProductCard from "../../components/ProductCard";
 import { Typography, Container } from "@mui/material";
+import { motion } from "framer-motion";
 import * as backend from "../../api/backend.js";
 
 function Results() {
@@ -16,17 +17,50 @@ function Results() {
   return (
     <>
       <Typography
-        sx={{ fontWeight: "medium", padding: "1rem" }}
+        sx={{
+          fontWeight: "medium",
+          padding: "1rem",
+          textAlign: "center",
+          marginTop: "3rem",
+        }}
         variant="h4"
         component="h1"
       >
-        Recommended Meta AI
+        How Meta AI Can Help You
+      </Typography>
+      <Typography
+        sx={{
+          textAlign: "center",
+          color: "text.secondary",
+          width: "37rem",
+          margin: "0 auto 2rem auto",
+        }}
+      >
+        We&apos;ve curated a list of Meta AI features that will help you and
+        your business thrive. Browse, learn more, or head over to our user
+        stories to see how these Meta AI features have succesfully helped
+        others.
       </Typography>
       <Container
-        sx={{ display: "flex", flexWrap: "wrap", gap: "1rem", padding: "1rem" }}
+        sx={{
+          width: "100%",
+          display: "flex",
+          flexWrap: "wrap",
+          gap: "3rem",
+          padding: "1rem",
+          justifyContent: "center",
+        }}
       >
-        {productList.map((product) => (
-          <ProductCard key={product.key} product={product} />
+        {productList.map((product, index) => (
+          <motion.div
+            key={product.key}
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 50 }}
+            transition={{ duration: 0.5, delay: index * 0.2 }}
+          >
+            <ProductCard key={product.key} product={product} />
+          </motion.div>
         ))}
       </Container>
     </>
