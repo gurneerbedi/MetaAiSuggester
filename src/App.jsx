72529cd7ec9b019f49ced0769bbd1461b73497d9
bzhang98 <105823390+bzhang98@users.noreply.github.com>
2024-11-21 23:23:33 -0600

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Quiz from "./pages/Quiz";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/quiz" element={<Quiz />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
