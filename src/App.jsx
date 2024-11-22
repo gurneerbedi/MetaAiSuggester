95460ef0fe7592692b02f49e03ff7cd57b1a1378
Emily Schur <ejschur@gmail.com>
2024-11-21 21:36:40 -0800

    Add routing, initial card styling, backend api file

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-import * as React from "react";
-import cardImage from "../assets/images/placeholder-card-img.jpg";
-import {
-  CardActionArea,
-  Button,
-  Typography,
-  CardMedia,
-  CardContent,
-  CardActions,
-  Card,
-  createTheme,
-  ThemeProvider,
-} from "@mui/material";
+import Landing from "./Pages/Landing/Landing.jsx";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Results from "./Pages/Results/Results.jsx";
+import Explore from "./Pages/Explore/Explore.jsx";
 
 const theme = createTheme({
   typography: {
@@ -27,48 +18,15 @@ const theme = createTheme({
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Card sx={{ maxWidth: 320, borderRadius: "20px" }}>
-        <CardMedia sx={{ height: 225 }} image={cardImage} title="meta ai" />
-        <CardContent sx={{ padding: "1.5rem 1.5rem 0.5rem 1.5rem" }}>
-          <Typography
-            gutterBottom
-            variant="h5"
-            component="h3"
-            sx={{ fontWeight: "medium" }}
-          >
-            Product Name
-          </Typography>
-          <Typography variant="body2" sx={{ color: "text.secondary" }}>
-            Product description: Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Modi debitis cum quo voluptates quis ex autem
-            repellendus ea numquam maxime suscipit.
-          </Typography>
-        </CardContent>
-        <CardActions sx={{ padding: "0.5rem 1rem 1rem 1rem" }}>
-          <Button
-            sx={{
-              textTransform: "capitalize",
-              fontSize: "14px",
-              padding: "8px 20px",
-              borderRadius: "24px",
-            }}
-            size="small"
-            variant="contained"
-            disableElevation
-          >
-            Learn More
-          </Button>
-        </CardActions>
-      </Card>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/results" element={<Results />} />
+          <Route path="/explore" element={<Explore />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
-=======
-import LandingPage from "./Pages/Landing/Landing";
-
-function App() {
-  return <LandingPage/>
-
->>>>>>> develop
 }
 
 export default App;
